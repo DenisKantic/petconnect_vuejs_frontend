1deb3b9e63cf44d3b8be407e4dc8553d8ece0616
DenisKantic <denis.kantic18@gmail.com>
2025-01-22 21:22:26 +0100

    created logic for switching menu with login button in navbar. Feels buggy need to test it more

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,20 @@
 import {jwtDecode} from 'jwt-decode';
 import Cookies from 'js-cookie';
 import axios from 'axios';
+import { ref } from 'vue';
 
 const BACKGROUND_VALIDATION_INTERVAL = 5 * 60 * 1000; // 5 minutes
 
+const isLoggedIn = ref(false);
+
+export {isLoggedIn}
 
 export default function auth() {
   return new Promise((resolve, reject) => {
     const token = Cookies.get('auth_token');
 
     if (!token) {
+      isLoggedIn.value = false;
       reject(new Error('No token found'));
       return;
     }
@@ -20,16 +25,19 @@ export default function auth() {
 
       if (decodedToken.exp < currentTime) {
         Cookies.remove('auth_token');
+        isLoggedIn.value = false;
         reject(new Error('Token expired'));
         return;
       }
 
       // If token is valid, proceed and set up background validation
       setupBackgroundValidation(token);
+      isLoggedIn.value = true;
       resolve();
     } catch (error) {
       console.error('Error decoding token:', error);
       Cookies.remove('auth_token');
+      isLoggedIn.value = false;
       reject(error);
     }
   });
@@ -45,12 +53,12 @@ function setupBackgroundValidation(token) {
 
       if (!response.data.valid) {
         Cookies.remove('auth_token');
-        window.location.href = '/prijava'; // Redirect to login page
+        this.$router.push('/prijava')
       }
     } catch (error) {
       console.error('Background validation error:', error);
       Cookies.remove('auth_token');
-      window.location.href = '/prijava'; // Redirect to login page
+      this.$router.push('/prijava')      // Redirect to login page
     }
   }, BACKGROUND_VALIDATION_INTERVAL);
 }
