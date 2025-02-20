bd10fd4fda80dc86d9736aee7953535bcbef7a7b
DenisKantic <denis.kantic18@gmail.com>
2025-02-20 14:38:41 +0100

    FIX in auth register, NEW things in settings component

--- src/components/settings/settings.vue
+++ src/components/settings/settings.vue
@@ -1,6 +1,6 @@
 <template>
-  <fieldset class="px-16 mt-10" id="container" style="width: 60%; margin: auto">
-    <legend class="text-h5">Postavke profila</legend>
+  <v-card :disabled="passwordCardLoading" :loading="passwordCardLoading" class="px-5 py-5 mt-10" id="container" >
+    <v-card-title class="px-0 pb-4 text-h5">Promijeni šifru</v-card-title>
     <label>Nova šifra</label>
     <v-text-field
       class="my-2"
@@ -31,24 +31,79 @@
       }}</v-icon></v-text-field
     >
 
-    <v-btn color="primary">Promijeni</v-btn>
+    <v-btn color="primary" @click="submit_password" :loading="changePassBtnLoading">Promijeni</v-btn>
+    </v-card>
 
-    <v-divider class="pb-4 mt-4"></v-divider>
+    <v-card id="container" class="mt-10" color="error">
+      <v-card-title class="d-flex items-center">Obriši profil
+      </v-card-title>
 
-    <p class="text-h6">Obriši korisnički profil</p>
+      <v-card-text>Obrišite vaš profil u potpunosti sa svim oglasima i server podacima
+        <br><span>PAŽNJA: Ovaj postupan je nepovratan!</span>
+      </v-card-text>
+      <v-dialog max-width="500">
+  <template v-slot:activator="{ props: activatorProps }">
+    <v-btn
+      class="ml-4 mb-4"
+      v-bind="activatorProps"
+      color="red"
+      variant="flat"
+    >
+    Obriši profil
+    <v-icon size="25" class="ml-2">mdi-alert</v-icon>
+  </v-btn>
+  </template>
 
-    <v-btn color="red" class="mb-5">Obriši</v-btn>
-  </fieldset>
+  <template v-slot:default="{ isActive }">
+    <v-card  title="Obriši profil">
+      <v-card-text>
+        Da li ste sigurni da želite obrisati profil kao i sve podatke?
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn
+        text="Obriši"
+        color="error"
+        variant="flat"
+        ></v-btn>
+        <v-btn
+        color="primary"
+          text="Odustani"
+          variant="flat"
+          @click="isActive.value = false"
+        ></v-btn>
+      </v-card-actions>
+    </v-card>
+  </template>
+</v-dialog>
+     
+    </v-card>
+    <v-snackbar
+      v-model="snackbar.visible"
+      :timeout="snackbar.timeout"
+      :color="snackbar.color"
+    >
+      {{ snackbar.message }}</v-snackbar>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      snackbar: {
+        visible: false,
+        message: "",
+        timeout: 3000, // Duration in milliseconds
+        color: "success", // The color of the snackbar (success, error, etc.)
+      },
       show_password: false,
       show_check_pass: false,
       text: "",
       password: "",
+      changePassBtnLoading: false,
+      passwordCardLoading: false,
       confirm_password: "",
       rules: {
         required: (value) => !!value || "Obavezno polje",
@@ -75,6 +130,41 @@ export default {
     toggle_check_pass() {
       this.show_check_pass = !this.show_check_pass;
     },
+    showSnackbar(message, color) {
+      this.snackbar.visible = true;
+      this.snackbar.message = message;
+      this.snackbar.color = color;
+    },
+
+    submit_password(){
+
+      this.changePassBtnLoading = true;
+      this.passwordCardLoading = true;
+      if (this.password != this.confirm_password){
+        this.showSnackbar("Šifre se ne poklapaju", "error")
+        this.changePassBtnLoading = false;
+        this.passwordCardLoading = false;
+        return
+      }
+
+      const params = {
+        password: this.password
+      }
+
+      this.$http.post('http://localhost:8080/change-password', params, {
+        withCredentials: true
+      })
+      .then((response)=>{
+        console.log("RESPONSE HERRE",response.data.success)
+        this.showSnackbar(response.data.success, "success")
+      })
+      .catch((error)=>{
+        this.showSnackbar(response.data.error, "error")
+      })
+
+      this.changePassBtnLoading = false
+      this.passwordCardLoading = false;
+    }
   },
 };
 </script>
@@ -95,16 +185,13 @@ export default {
   position: relative;
 }
 
-#container {
-  border: 1px solid black;
-}
 
 label {
   font-size: 1.2rem;
 }
 @media (max-width: 1024px) {
   .v-form {
-    width: 90% !important;
+    width: 60% !important;
   }
 
   .text-h4 {
@@ -112,7 +199,16 @@ label {
   }
 
   #container {
-    width: 90% !important;
+    width: 80% !important;
+    background-color: aqua;
+    margin: auto;
+  }
+}
+
+@media (min-width: 1025px){
+  #container{
+    width: 50%;
+    margin: auto;
   }
 }
 </style>
